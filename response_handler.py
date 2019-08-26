34a15f9ccfbabd02792ecc1cc052af9392d7cf3d
farah-afifi <farahmaghraby1999@gmail.com>
2019-08-26 20:30:56 +0200

    averted the expired session exception by creating a new session every time this happens

--- response_handler.py
+++ response_handler.py
@@ -8,17 +8,32 @@ class response_handler():
 		self.assistant_id = assistant_id
 		self.service = service
 	
+	def __init__ (self,service,assistant_id ,session_id):
+		self.session_id = session_id
+		self.assistant_id = assistant_id
+		self.service = service
+	
+	def get_watson_response(self ,msg):
+		response = self.service.message(
+				assistant_id=self.assistant_id,
+				session_id= self.session_id,
+				input={
+					'message_type': 'text',
+					'text': msg
+				}
+			).get_result()
+		return response
+		
 	def make_reply(self, msg):
 		if msg is not None:
-			response = self.service.message(
-			assistant_id=self.assistant_id,
-			session_id= self.session_id,
-			input={
-				'message_type': 'text',
-				'text': msg
-			}
-			).get_result()
-
+			try:			
+				response = self.get_watson_response(msg)
+			except:
+				session = self.service.create_session(self.assistant_id).get_result()
+				self.session_id = session['session_id']
+				response = self.get_watson_response(msg)
+				
+	
 		if len(response["output"]["intents"]) > 0 and response["output"]["intents"][0]['intent'] != 'Google_Custom_Search':
 			return response["output"]["generic"][0]['text']
 		else:
